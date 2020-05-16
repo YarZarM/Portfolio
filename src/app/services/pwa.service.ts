7f8a79659c5d60fd05ad873ffd8cb6b647600325
Yar Zar Moe Myint <yzmm95@gmail.com>
2020-05-17 00:47:08 +0300

    add pwa install test

---
+++ src/app/services/pwa.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PwaService {
+
+  public promptEvent: any;
+      
+  constructor() {
+    window.addEventListener('beforeinstallprompt', event => {
+        this.promptEvent = event;
+      });
+  }
+}
